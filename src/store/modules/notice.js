2663a0d35c5d4474bdae20d0367e4acd740633d3
xupk <1099656153@qq.com>
2019-05-07 14:25:38 +0800

    创建+管理+详情

---
+++ src/store/modules/notice.js
@@ -0,0 +1,52 @@
+import Mock from 'mockjs'
+
+const noticeList = []
+const count = 100
+
+for (let i = 0; i < count; i++) {
+  noticeList.push(Mock.mock({
+    editIf: true,
+    noticeIfRead: false,
+    noticeId: '@increment',
+    noticeDate: '@date',
+    'noticePublish|1': [
+      { publishId: 1, typePublish: '人力资源部' },
+      { publishId: 2, typePublish: '办公厅' },
+      { publishId: 3, typePublish: '综合部' }],
+    noticeTitle: '@ctitle(7,14)',
+    noticeContent: '@cparagraph(25, 40)',
+    noticeReadNum: '@integer(20, 300)',
+    'noticeStatus|1': ['published', 'draft']
+  }))
+}
+
+const state = {
+  notice: noticeList
+}
+
+const mutations = {
+
+}
+const actions = {
+  addNotice({ commit }, data) {
+    state.notice.unshift(data)
+  },
+  updateNotice({ commit }, data) {
+    for (const v of state.notice) {
+      if (v.noticeId === data.noticeId) {
+        const index = state.notice.indexOf(v)
+        state.notice.splice(index, 1, data)
+        break
+      }
+    }
+  },
+  deleteNotice({ commit }, row) {
+    const index = state.notice.indexOf(row)
+    state.notice.splice(index, 1)
+  }
+}
+export default ({
+  state,
+  mutations,
+  actions
+})
