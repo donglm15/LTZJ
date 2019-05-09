dba0e3de9201451c843131c673d13f3734454978
zhanglx201 <zhanglx201@chinaunicom.cn>
2019-05-09 09:44:25 +0800

    5.8

--- mock/index.js
+++ mock/index.js
@@ -5,12 +5,14 @@ import user from './user'
 import role from './role'
 import article from './article'
 import search from './remote-search'
+import info from './info'
 
 const mocks = [
   ...user,
   ...role,
   ...article,
-  ...search
+  ...search,
+  ...info
 ]
 
 // for front mock
